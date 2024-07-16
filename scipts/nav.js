7b9db5d752fd4985e24df3db1b96ed2367c5142c
emohawk13 <dauhson13@gmail.com>
2024-07-16 15:19:21 -0600

    refactor commit

--- scipts/nav.js
+++ scipts/nav.js
@@ -1,14 +1,14 @@
-var nav = document.querySelector('.nav');
-var navHeight = nav.clientHeight; // Get the height of the navigation bar
+var nav = document.querySelector("nav");
+var navHeight = nav.clientHeight;
 
 function checkNavPosition() {
-    var scrollPosition = window.scrollY;
+  var scrollPosition = window.scrollY;
 
-    if (scrollPosition > navHeight) {
-        nav.classList.add('scrolled');
-    } else {
-        nav.classList.remove('scrolled');
-    }
+  if (scrollPosition > navHeight) {
+    nav.classList.add("scrolled");
+  } else {
+    nav.classList.remove("scrolled");
+  }
 }
 
-window.addEventListener('scroll', checkNavPosition);
+window.addEventListener("scroll", checkNavPosition);
